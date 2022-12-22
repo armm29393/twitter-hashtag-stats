637fdf53b664a75169f3204c8ad53f130df48819
Pawarit Thongaek <arm.techhitz@gmail.com>
2022-12-22 16:04:34 +0700

    🐞 fix: time display

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from thai_strftime import thai_strftime
 from config import *
 
-endDate = datetime.utcnow().replace(tzinfo=pytz.UTC) - timedelta(seconds=10)
+endDate = datetime.utcnow().replace(tzinfo=pytz.UTC)
 startDate = endDate - timedelta(hours=16)
 localDate = endDate.astimezone(pytz.timezone("Asia/Bangkok"))
 
@@ -12,7 +12,7 @@ print('endDate>>', endDate)
 
 for s in config['data']:
     print(f'Getting {s["title"]} stats...')
-    data_all, data_tweet, data_retweet = search(s['q'], startDate, endDate)
+    data_all, data_tweet, data_retweet = search(s['q'], startDate, endDate - timedelta(seconds=10))
     status = f'{config["header"]}\n\n'
     status += f'วันที่ {thai_strftime(localDate, fmt="%-d %b %Y %H:%M", buddhist_era=False)}\n'
     status += f'{s["type"].capitalize()}: {s["title"]}\n\n'
